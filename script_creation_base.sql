176e429613f122111b91448a9f0fae14cad9bc77
Bjall <bjallet@gmail.com>
2017-10-17 17:49:10 +0200

    Add files via upload

--- script_creation_base.sql
+++ script_creation_base.sql
@@ -4,7 +4,7 @@
 
 
 #------------------------------------------------------------
-# Table: civilité
+# Table: civilite
 #------------------------------------------------------------
 
 CREATE TABLE civilite(
@@ -73,7 +73,7 @@ CREATE TABLE veh_boite(
 
 CREATE TABLE veh_porte(
         veh_porte_id      int (11) Auto_increment  NOT NULL ,
-        veh_porte_libelle Varchar (50) NOT NULL ,
+        veh_porte_libelle Int NOT NULL ,
         PRIMARY KEY (veh_porte_id )
 )ENGINE=InnoDB;
 
@@ -106,7 +106,7 @@ CREATE TABLE vehm_marque(
 
 CREATE TABLE vehm_modele(
         vehmmod_id      int (11) Auto_increment  NOT NULL ,
-        vehmmod_libelle Varchar (50) NOT NULL ,
+        vehmmod_libelle Varchar (255) NOT NULL ,
         vehmar_id       Int NOT NULL ,
         PRIMARY KEY (vehmmod_id )
 )ENGINE=InnoDB;
@@ -130,7 +130,6 @@ CREATE TABLE veh_couleur(
 CREATE TABLE Vehicule(
         veh_id         int (11) Auto_increment  NOT NULL ,
         veh_date_achat Date NOT NULL ,
-        veh_prix_u_ht  Float NOT NULL ,
         veh_photo      Varchar (255) NOT NULL ,
         veh_nb_place   Int NOT NULL ,
         veh_assur      Varchar (50) NOT NULL ,
@@ -163,10 +162,10 @@ CREATE TABLE accessoire(
 
 
 #------------------------------------------------------------
-# Table: (tva)
+# Table: tva
 #------------------------------------------------------------
 
-CREATE TABLE _tva_(
+CREATE TABLE tva(
         tva_id   int (11) Auto_increment  NOT NULL ,
         tva_taux Float NOT NULL ,
         PRIMARY KEY (tva_id )
@@ -185,11 +184,13 @@ CREATE TABLE contratdelocation(
         contrat_date_debut       Date NOT NULL ,
         contrat_caution          Float ,
         contrat_date_reservation Date ,
+        contrat_accompte         Float ,
         cli_id                   Int NOT NULL ,
         tva_id                   Int NOT NULL ,
         veh_id                   Int NOT NULL ,
         fac_id                   Int ,
         forfait_id               Int NOT NULL ,
+        statut_cont_id           Int NOT NULL ,
         PRIMARY KEY (contrat_id )
 )ENGINE=InnoDB;
 
@@ -201,6 +202,7 @@ CREATE TABLE contratdelocation(
 CREATE TABLE type_veh(
         type_veh_id      int (11) Auto_increment  NOT NULL ,
         type_veh_libelle Varchar (50) NOT NULL ,
+        type_veh_prix    Float NOT NULL ,
         cat_id           Int NOT NULL ,
         PRIMARY KEY (type_veh_id )
 )ENGINE=InnoDB;
@@ -285,8 +287,9 @@ CREATE TABLE cpville(
 #------------------------------------------------------------
 
 CREATE TABLE option_contrat(
-        option_cont_id      int (11) Auto_increment  NOT NULL ,
-        option_cont_libelle Varchar (30) NOT NULL ,
+        option_cont_id              int (11) Auto_increment  NOT NULL ,
+        option_cont_libelle         Varchar (30) NOT NULL ,
+        option_cont_prix_journalier Float NOT NULL ,
         PRIMARY KEY (option_cont_id )
 )ENGINE=InnoDB;
 
@@ -308,10 +311,9 @@ CREATE TABLE Facture(
 #------------------------------------------------------------
 
 CREATE TABLE statut_etat_des_lieux(
-        stat_id      int (11) Auto_increment  NOT NULL ,
-        stat_libelle Varchar (25) NOT NULL ,
-        entr_id      Int NOT NULL ,
-        PRIMARY KEY (stat_id )
+        stat_etat__id     int (11) Auto_increment  NOT NULL ,
+        stat_etat_libelle Varchar (25) NOT NULL ,
+        PRIMARY KEY (stat_etat__id )
 )ENGINE=InnoDB;
 
 
@@ -320,12 +322,13 @@ CREATE TABLE statut_etat_des_lieux(
 #------------------------------------------------------------
 
 CREATE TABLE entretien(
-        entr_id      int (11) Auto_increment  NOT NULL ,
-        entr_libelle Varchar (50) NOT NULL ,
-        entr_statut  Varchar (50) NOT NULL ,
-        entr_date    Date NOT NULL ,
-        pers_id      Int NOT NULL ,
-        veh_id       Int NOT NULL ,
+        entr_id             int (11) Auto_increment  NOT NULL ,
+        entr_libelle        Varchar (50) NOT NULL ,
+        entr_date           Date NOT NULL ,
+        stat_etat__id       Int NOT NULL ,
+        pers_id             Int NOT NULL ,
+        veh_id              Int NOT NULL ,
+        statut_entretien_id Int NOT NULL ,
         PRIMARY KEY (entr_id )
 )ENGINE=InnoDB;
 
@@ -405,15 +408,26 @@ CREATE TABLE Type_Adresse(
 
 
 #------------------------------------------------------------
-# Table: Pénalités
+# Table: penalites
 #------------------------------------------------------------
 
-CREATE TABLE Penalites(
-        idLibellePenalite int (11) Auto_increment  NOT NULL ,
-        LIbellePenalite   Varchar (25) ,
-        Forfait           Float ,
-        Coefficient       Float ,
-        PRIMARY KEY (idLibellePenalite )
+CREATE TABLE penalites(
+        penalite_id      int (11) Auto_increment  NOT NULL ,
+        penalite_libelle Varchar (50) NOT NULL ,
+        penalite_forfait Float ,
+        penalite_coeff   Float ,
+        PRIMARY KEY (penalite_id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: statut_entretien
+#------------------------------------------------------------
+
+CREATE TABLE statut_entretien(
+        statut_entretien_id      int (11) Auto_increment  NOT NULL ,
+        statut_entretien_libelle Varchar (50) NOT NULL ,
+        PRIMARY KEY (statut_entretien_id )
 )ENGINE=InnoDB;
 
 
@@ -467,32 +481,21 @@ CREATE TABLE ajouter(
 #------------------------------------------------------------
 
 CREATE TABLE sensuivre(
-        stat_id           Int NOT NULL ,
+        stat_etat__id     Int NOT NULL ,
         etat_des_lieux_id Int NOT NULL ,
         element_id        Int NOT NULL ,
-        PRIMARY KEY (stat_id ,etat_des_lieux_id ,element_id )
+        PRIMARY KEY (stat_etat__id ,etat_des_lieux_id ,element_id )
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: etatducontrat
+# Table: penaliser
 #------------------------------------------------------------
 
-CREATE TABLE etatducontrat(
-        statut_cont_id Int NOT NULL ,
-        contrat_id     Int NOT NULL ,
-        PRIMARY KEY (statut_cont_id ,contrat_id )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: relation3
-#------------------------------------------------------------
-
-CREATE TABLE relation3(
-        contrat_id        Int NOT NULL ,
-        idLibellePenalite Int NOT NULL ,
-        PRIMARY KEY (contrat_id ,idLibellePenalite )
+CREATE TABLE penaliser(
+        contrat_id  Int NOT NULL ,
+        penalite_id Int NOT NULL ,
+        PRIMARY KEY (contrat_id ,penalite_id )
 )ENGINE=InnoDB;
 
 ALTER TABLE client ADD CONSTRAINT FK_client_cli_stat_id FOREIGN KEY (cli_stat_id) REFERENCES statut_client(cli_stat_id);
@@ -506,18 +509,20 @@ ALTER TABLE Vehicule ADD CONSTRAINT FK_Vehicule_vehmmod_id FOREIGN KEY (vehmmod_
 ALTER TABLE Vehicule ADD CONSTRAINT FK_Vehicule_veh_porte_id FOREIGN KEY (veh_porte_id) REFERENCES veh_porte(veh_porte_id);
 ALTER TABLE Vehicule ADD CONSTRAINT FK_Vehicule_typepermis_id FOREIGN KEY (typepermis_id) REFERENCES type_permis(typepermis_id);
 ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_cli_id FOREIGN KEY (cli_id) REFERENCES client(cli_id);
-ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_tva_id FOREIGN KEY (tva_id) REFERENCES _tva_(tva_id);
+ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_tva_id FOREIGN KEY (tva_id) REFERENCES tva(tva_id);
 ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_veh_id FOREIGN KEY (veh_id) REFERENCES Vehicule(veh_id);
 ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_fac_id FOREIGN KEY (fac_id) REFERENCES Facture(fac_id);
 ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_forfait_id FOREIGN KEY (forfait_id) REFERENCES forfait(forfait_id);
+ALTER TABLE contratdelocation ADD CONSTRAINT FK_contratdelocation_statut_cont_id FOREIGN KEY (statut_cont_id) REFERENCES statut_contrat(statut_cont_id);
 ALTER TABLE type_veh ADD CONSTRAINT FK_type_veh_cat_id FOREIGN KEY (cat_id) REFERENCES categorie_veh(cat_id);
 ALTER TABLE etat_des_lieux ADD CONSTRAINT FK_etat_des_lieux_pers_id FOREIGN KEY (pers_id) REFERENCES personnel(pers_id);
 ALTER TABLE etat_des_lieux ADD CONSTRAINT FK_etat_des_lieux_contrat_id FOREIGN KEY (contrat_id) REFERENCES contratdelocation(contrat_id);
 ALTER TABLE personnel ADD CONSTRAINT FK_personnel_fonc_id FOREIGN KEY (fonc_id) REFERENCES fonction(fonc_id);
 ALTER TABLE cpville ADD CONSTRAINT FK_cpville_pays_id FOREIGN KEY (pays_id) REFERENCES Pays(pays_id);
-ALTER TABLE statut_etat_des_lieux ADD CONSTRAINT FK_statut_etat_des_lieux_entr_id FOREIGN KEY (entr_id) REFERENCES entretien(entr_id);
+ALTER TABLE entretien ADD CONSTRAINT FK_entretien_stat_etat__id FOREIGN KEY (stat_etat__id) REFERENCES statut_etat_des_lieux(stat_etat__id);
 ALTER TABLE entretien ADD CONSTRAINT FK_entretien_pers_id FOREIGN KEY (pers_id) REFERENCES personnel(pers_id);
 ALTER TABLE entretien ADD CONSTRAINT FK_entretien_veh_id FOREIGN KEY (veh_id) REFERENCES Vehicule(veh_id);
+ALTER TABLE entretien ADD CONSTRAINT FK_entretien_statut_entretien_id FOREIGN KEY (statut_entretien_id) REFERENCES statut_entretien(statut_entretien_id);
 ALTER TABLE Adresse ADD CONSTRAINT FK_Adresse_cli_id FOREIGN KEY (cli_id) REFERENCES client(cli_id);
 ALTER TABLE Adresse ADD CONSTRAINT FK_Adresse_type_adr_id FOREIGN KEY (type_adr_id) REFERENCES Type_Adresse(type_adr_id);
 ALTER TABLE Adresse ADD CONSTRAINT FK_Adresse_cp_id FOREIGN KEY (cp_id) REFERENCES cpville(cp_id);
@@ -529,10 +534,8 @@ ALTER TABLE disposer ADD CONSTRAINT FK_disposer_option_veh_id FOREIGN KEY (optio
 ALTER TABLE disposer ADD CONSTRAINT FK_disposer_veh_id FOREIGN KEY (veh_id) REFERENCES Vehicule(veh_id);
 ALTER TABLE ajouter ADD CONSTRAINT FK_ajouter_contrat_id FOREIGN KEY (contrat_id) REFERENCES contratdelocation(contrat_id);
 ALTER TABLE ajouter ADD CONSTRAINT FK_ajouter_option_cont_id FOREIGN KEY (option_cont_id) REFERENCES option_contrat(option_cont_id);
-ALTER TABLE sensuivre ADD CONSTRAINT FK_sensuivre_stat_id FOREIGN KEY (stat_id) REFERENCES statut_etat_des_lieux(stat_id);
+ALTER TABLE sensuivre ADD CONSTRAINT FK_sensuivre_stat_etat__id FOREIGN KEY (stat_etat__id) REFERENCES statut_etat_des_lieux(stat_etat__id);
 ALTER TABLE sensuivre ADD CONSTRAINT FK_sensuivre_etat_des_lieux_id FOREIGN KEY (etat_des_lieux_id) REFERENCES etat_des_lieux(etat_des_lieux_id);
 ALTER TABLE sensuivre ADD CONSTRAINT FK_sensuivre_element_id FOREIGN KEY (element_id) REFERENCES element_vehicule_etat(element_id);
-ALTER TABLE etatducontrat ADD CONSTRAINT FK_etatducontrat_statut_cont_id FOREIGN KEY (statut_cont_id) REFERENCES statut_contrat(statut_cont_id);
-ALTER TABLE etatducontrat ADD CONSTRAINT FK_etatducontrat_contrat_id FOREIGN KEY (contrat_id) REFERENCES contratdelocation(contrat_id);
-ALTER TABLE relation3 ADD CONSTRAINT FK_relation3_contrat_id FOREIGN KEY (contrat_id) REFERENCES contratdelocation(contrat_id);
-ALTER TABLE relation3 ADD CONSTRAINT FK_relation3_idLibellePenalite FOREIGN KEY (idLibellePenalite) REFERENCES Penalites(idLibellePenalite);
+ALTER TABLE penaliser ADD CONSTRAINT FK_penaliser_contrat_id FOREIGN KEY (contrat_id) REFERENCES contratdelocation(contrat_id);
+ALTER TABLE penaliser ADD CONSTRAINT FK_penaliser_penalite_id FOREIGN KEY (penalite_id) REFERENCES penalites(penalite_id);
--- script_insertion_donnees.sql
+++ script_insertion_donnees.sql
@@ -6,7 +6,7 @@ INSERT INTO statut_client(cli_stat_id, cli_stat_libelle, cli_stat_taux) VALUES
 (1, 'Particulier', 1),
 (2,'Professionnel', 0.90);
 
-INSERT INTO civilite (cli_civ_id, cli_civ_libelle, cli_civ_denomination) VALUES
+INSERT INTO civilite(cli_civ_id, cli_civ_libelle, cli_civ_denomination) VALUES
 (1, 'Mme', 'Madame'),
 (2, 'M', 'Monsieur');
 
@@ -23,7 +23,12 @@ INSERT INTO type_adresse(type_adr_libelle) VALUES
 ('facturation');
 
 INSERT INTO pays(pays_libelle, pays_code) VALUES
-('France', 'FR');
+('FRANCE', 'FR'),
+('ALLEMAGNE', 'DE'),
+('ESPAGNE', 'ES'),
+('ITALIE', 'IT'),
+('BELGIQUE', 'BE'),
+('ROYAUME-UNI', 'GB');
 
 INSERT INTO cpville(cp_ville, cp_codepostal, pays_id) VALUES
 ('Les Sables-d''Olonne', '85100', 1),
@@ -82,3 +87,221 @@ INSERT INTO detenir(cli_id, typepermis_id) VALUES
 (10, 2),
 (11, 3),
 (12, 5);
+
+INSERT INTO categorie_veh(cat_libelle) VALUES 
+('Vélo'),
+('Motos'),
+('Voiture'),
+('Utilitaire'),
+('Van à chevaux');
+
+INSERT INTO type_veh(type_veh_libelle, cat_id, type_veh_prix) VALUES
+('moto_sportive', 2, 160),
+('moto_routiere', 2, 140), 
+('moto_trail', 2, 120), 
+('moto_roadster', 2, 100),
+('VTC', 1, 10),
+('VTT', 1, 15),
+('Tandem', 1, 20),
+('Scooter',  2, 40),
+('SUV',  3, 75),
+('Berline', 3, 60),
+('Citadine', 3, 40),
+('Familiale', 3, 80),
+('Sportive', 3, 120),
+('Décapotable', 3, 140);
+
+INSERT INTO veh_boite(veh_boite_libelle) VALUES
+('Manuelle'), 
+('Automatique');
+
+INSERT INTO veh_type_carburant(vehm_carb_libelle) VALUES
+('Essence'), 
+('Gazole'), 
+('GPL'), 
+('Electrique'), 
+('Hybride');
+
+INSERT INTO veh_couleur(veh_coul_libelle) VALUES
+('rouge'),
+('bleu'),
+('vert'),
+('jaune'),
+('marron'),
+('noir'),
+('blanc'),
+('gris'),
+('violet'),
+('bordeaux'),
+('noir/orange/blanc');
+
+INSERT INTO vehm_marque(vehmar_libelle) VALUES
+('Alfa Romeo'), 
+('Audi'), 
+('BMW'), 
+('Citroën'), 
+('Ferrari'), 
+('Fiat'), 
+('Ford'), 
+('Honda'), 
+('Jaguar'), 
+('Mercedes'), 
+('Mini'), 
+('Peugeot'), 
+('Renault'), 
+('SEAT'), 
+('Toyota'), 
+('Wolkswagen'), 
+('Volvo'),
+('Suzuki');
+
+INSERT INTO vehm_modele(vehmmod_libelle, vehmar_id) VALUES
+('KA 2016 II 1.2 69 S&S BLACK EDITION', 7),
+('CLIO 2013 IV 1.5 DCI 90 ENERGY DYNAMIQUE ECO2 90G', 13),
+('C5 2016 II (2) 2.0 BLUEHDI 150 S&S CONFORT BV6 111Go', 4),
+('STELVIO 2017 2.0 TURBO 200 Q4 LUSSO AT8', 1),
+('CLASSE E 2017 IV (2) CABRIOLET 200 EXECUTIVE BVA7', 10),
+('eC 01 D9', 12),
+('750 GSX-R 2010', 18);
+
+INSERT INTO veh_porte(veh_porte_libelle) VALUES 
+(2),
+(3),
+(5);
+
+INSERT INTO option_veh(option_veh_libelle) VALUES
+('climatisation'),
+('siege chauffant'),
+('siege cuir'),
+('volant chauffant'),
+('poignee chauffante');
+
+INSERT INTO vehicule(veh_date_achat, veh_photo, veh_nb_place, veh_assur, veh_date_mec, veh_kmage, veh_ch_fisc, veh_ch_reel, veh_cyl, type_veh_id, veh_boite_id, vehm_carb_id, veh_coul_id, vehmmod_id, veh_porte_id, typepermis_id) VALUES
+('2017-03-19', '../images/photos/vehicule1.png', 4, 'F24415225555', '2017-03-19', 24630, 4, 90, 1242, 11, 1, 1, 8, 1, 1, 4),
+('2016-11-10', '../images/photos/vehicule2.png', 5, 'F24414627653', '2016-11-10', 46527, 4, 90, 1461, 11, 1, 2, 1, 2, 2, 4),
+('2016-08-23', '../images/photos/vehicule3.png', 5, 'F24417628652', '2016-08-23', 68426, 8, 150, 1997, 10, 1, 2, 2, 3, 3, 4),
+('2017-01-21', '../images/photos/vehicule4.png', 5, 'F24484622983', '2017-01-21', 12378, 12, 200, 1995, 9, 1, 1, 5, 4, 3, 4),
+('2017-03-19', '../images/photos/vehicule5.png', 5, 'F24417627612', '2017-03-19', 24630, 10, 184, 1242, 14, 2, 1, 10, 5, 1, 4),
+('2017-03-19', '../images/photos/vehicule5.png', 5, 'F24417627618', '2017-03-19', 18657, 10, 184, 1242, 14, 2, 1, 4, 5, 1, 4),
+('2015-09-13', '../images/photos/vehicule7.png', 1, 'F24415715223', NULL, NULL, NULL, NULL, NULL, 5, NULL, NULL, 2, 6, NULL, NULL),
+('2016-09-28', '../images/photos/vehicule12.png', 2, 'F24415478188', '2016-09-28', 54826, 7, 150, 749, 1, 1, 1, 2, 7, NULL, 3);
+
+INSERT INTO disposer(option_veh_id, veh_id) VALUES
+(1, 2),
+(1, 3),
+(1, 4),
+(3, 5);
+
+INSERT INTO tva(tva_id, tva_taux) VALUES
+(1, '19.6'), 
+(2, '20');
+
+INSERT INTO forfait(forfait_libelle, forfait_taux) VALUES
+('Demi journée', 1.2),
+('Un jour Hors Week-end', 1),
+('Deux jours Hors Week-End', 0.94),
+('Trois Jours Hors Week-End', 0.92),
+('Semaine Hors Week-End', 0.88),
+('Semaine Complète', 0.89),
+('Week-End', 1.25),
+('Mois', 0.75);
+
+INSERT INTO facture(fac_montant, fac_date) VALUES
+(200,'2017-10-17'),
+(220,'2017-10-16'),
+(250,'2017-09-15'),
+(300,'2017-08-14'),
+(2500,'2017-07-13');
+
+INSERT INTO statut_contrat(statut_cont_libelle) VALUES
+('non signé'),
+('signé'),
+('en attente'),
+('cloturé');
+
+INSERT INTO option_contrat(option_cont_libelle, option_cont_prix_journalier) VALUES
+('conducteur supplémentaire', 8),
+('rachat franchise ttc', 15);
+
+INSERT INTO accessoire(acc_libelle, acc_prix_u_ht) VALUES
+('Siège auto', 15), 
+('Réhausseur', 10), 
+('GPS', 5), 
+('Casque vélo adulte', 5), 
+('Casque moto', 30), 
+('Casque vélo enfant', 2), 
+('Gant moto', 15), 
+('Blouson moto', 20), 
+('Remorque vélo simple', 20), 
+('Remorque vélo double', 30);
+
+INSERT INTO contratdelocation(contrat_date, contrat_lieu, contrat_date_fin, contrat_date_debut, contrat_caution, contrat_date_reservation, cli_id, tva_id, veh_id, fac_id, forfait_id, contrat_accompte, statut_cont_id) VALUES
+(NULL, 'Bordeaux', '2017-10-23', '2017-10-20', NULL, '2017-10-15', 1, 2, 1, NULL, 6, 220, 1),
+('2017-09-20', 'Bordeaux', '2017-09-23', '2017-09-20', 200, NULL, 2, 2, 2, 1, 5, NULL, 3),
+('2017-08-20', 'Bordeaux', '2017-08-23', '2017-08-20', NULL, '2017-08-20', 3, 2, 3, 2, 4, 400, 3),
+('2017-10-10', 'Bordeaux', '2017-10-22', '2017-10-13', NULL, '2017-10-13', 4, 2, 4, NULL, 2, 400, 2);
+
+INSERT INTO ajouter(contrat_id, option_cont_id) VALUES
+(1, 1),
+(1, 2),
+(2, 1);
+
+INSERT INTO rajouter(acc_id, contrat_id) VALUES
+(1, 3),
+(4, 3);
+
+INSERT INTO fonction(fonc_libelle) VALUES 
+('mécanicien'),
+('gestionnaire');
+
+INSERT INTO personnel(pers_nom, pers_prenom, pers_fonc_id) VALUES
+('Larry', 'Bambelle', 1),
+('Ray', 'Defesse', 2),
+('Jerry', 'Kan', 1),
+('Bart', 'Tabac', 1);
+
+INSERT INTO element_vehicule_etat(element_libelle, element_coefficient) VALUES
+('pneus', 1),
+('aile', 5),
+('rétroviseur', 0.1),
+('pare-brise', 1),
+('phare', 0.1);
+
+INSERT INTO statut_etat_des_lieux (stat_etat_libelle) VALUES
+('Fait'),
+('Non fait');
+
+INSERT INTO etat_des_lieux(etat_des_lieux_commentaire, etat_des_lieux_date, pers_id, contrat_id) VALUES
+('RAS', '2017-08-20', 4, 3),
+('Rétroviseur AVG cassé, Phare AVG', '2017-08-23', 3, 3),
+('RAS', '2017-09-20', 1, 2),
+('Réservoir vide', '2017-09-23', 1, 2),
+('RAS', '2017-10-13', 3, 4);
+
+INSERT INTO sensuivre(stat_etat__id, etat_des_lieux_id, element_id) VALUES
+(1, 1, NULL),
+(1, 2, 3),
+(1, 2, 4),
+(1, 3, NULL),
+(1, 4, NULL),
+(1, 5, NULL);
+
+INSERT INTO statut_entretien(statut_entretien_libelle) VALUES
+('fait'),
+('non fait'),
+('en cours');
+
+INSERT INTO entretien(entr_libelle,statut_entretient_id,entr_date) VALUES
+('nettoyage', 1, '2017-10-17'),
+('vidange', 2, '2017-09-12'),
+('changement ampoule', 3, '2017-10-16'),
+('changement essuis glace', 1, '2017-10-15');
+
+INSERT INTO penalites(penalite_libelle, penalite_forfait, penalite_coeff) VALUES
+('jours de retard', NULL, 2),
+('plein complet non fait', 100, NULL),
+('annulation de la resa avant 7jours', NULL, 0.2),
+('contravention', 50, NULL);
+
+INSERT INTO penaliser(contrat_id, penalite_id) VALUES
+(2, 2);
